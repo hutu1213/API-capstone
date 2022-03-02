0d4c61f112a3233429867cf5d9071593bd7dde44
hutu1213 <huytqse130537@fpt.edu.vn>
2022-03-02 20:23:54 +0700

    Update MailServiceImpl.java

--- src/main/java/project/apicapstone/service/impl/MailServiceImpl.java
+++ src/main/java/project/apicapstone/service/impl/MailServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import project.apicapstone.dto.mail.MailDto;
 import project.apicapstone.entity.Applicant;
@@ -21,12 +22,7 @@ import java.util.Properties;
 public class MailServiceImpl implements MailService {
     @Value("${spring.mail.username}")
     private String userName ;//= "huytq1899@gmail.com";
-    @Value("${spring.mail.password}")
-    private String password;// = "jwjrsvfazqwbjpcv";
-    @Value("${spring.mail.host}")
-    private String smtp;// = "smtp.gmail.com";
-    @Value("${spring.mail.port}")
-    private String port;// = "587";
+
     private JavaMailSender javaMailSender;
 
     public MailServiceImpl(JavaMailSender javaMailSender) {
@@ -36,43 +32,13 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void sendEmail(MailDto dto) throws MessagingException {
-
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", smtp);
-        properties.put("mail.smtp.port", port);
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-
-        Authenticator auth = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, password);
-            }
-        };
-        Session session = Session.getInstance(properties, auth);
-
-        MimeMessage msg = new MimeMessage(session);
-
-        msg.setFrom(new InternetAddress(userName));
-        InternetAddress[] toAddresses = { new InternetAddress(dto.getTo()) };
-        msg.setRecipients(Message.RecipientType.TO, toAddresses);
-        msg.setSubject(dto.getSubject(),"UTF-8");
-        msg.setText(dto.getBody(),"UTF-8");
-        msg.setContent(dto.getBody(), "text/html");
-
-        // sends the e-mail
-        Transport.send(msg);
-
-
-//        SimpleMailMessage message = new SimpleMailMessage();
-//
-//        message.setFrom("huytq1899@gmail.com");
-//        //message.setFrom(dto.getFrom());
-//        message.setTo(dto.getTo());
-//        message.setText(dto.getBody());
-//        message.setSubject(dto.getSubject());
-//
-//
-//        javaMailSender.send(message);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        helper.setText(dto.getBody(), true);
+        helper.setTo(dto.getTo());
+        helper.setSubject(dto.getSubject());
+        helper.setFrom(userName);
+        javaMailSender.send(mimeMessage);
     }
 
     @Override
